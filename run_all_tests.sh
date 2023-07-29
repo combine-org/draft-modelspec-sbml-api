b97c4b9e50e763059e2947315277919ade5da281
Robert Vickerstaff <r.vickerstaff@ucl.ac.uk>
2023-07-29 10:04:51 +0000

    run_all_tests.sh now checks if venv is active

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -4,6 +4,13 @@
 # run all tests
 #
 
+source ./includes
+
+if [[ "${VIRT_ENV}" != "${REPO_NAME}/${VIRTENV_NAME}" ]] ; then
+    echo Activate the virtual enviroment first using: source ./activate_venv.sh
+    exit 1
+fi
+
 mkdir -p untracked
 mkdir -p test/untracked
 
